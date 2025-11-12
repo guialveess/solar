139dc7d0a73295a961288035b1c21f03c1ae82c2
guialveess <97guilherme.alves@gmail.com>
2025-11-12 15:11:17 -0300

    fix: update pagetitle

--- components/pagetitle.tsx
+++ components/pagetitle.tsx
@@ -1,3 +1,3 @@
-export default function PageTitle({ children }) {
+export default function PageTitle({ children }: { children: React.ReactNode }) {
   return <h1 className="text-3xl font-sans mb-4 leading-tight">{children}</h1>;
 }
